0772bce30d17fcb40eb2541783b8dca3248aec5b
masoudrousta <mrousta2010@gmail.com>
2021-10-25 20:06:06 +0100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,34 @@
 import '../styles/globals.css'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth, db } from '../firebase'
+import { doc, setDoc } from 'firebase/firestore'
+
+import Login from '../components/Login'
+import Loading from '../components/Loading'
+import { useEffect } from 'react'
+import { serverTimestamp } from '@firebase/firestore'
 
 function MyApp({ Component, pageProps }) {
+  const [user, loading, error] = useAuthState(auth)
+
+  useEffect(() => {
+    if (user) {
+      setDoc(
+        doc(db, 'users', user.uid),
+        {
+          email: user.email,
+          lastSeen: serverTimestamp(),
+          photoURL: user.photoURL,
+        },
+        { merge: true }
+      )
+    }
+  }, [user])
+
+  if (loading) return <Loading />
+
+  if (!user) return <Login />
+
   return <Component {...pageProps} />
 }
 
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  switch (req.method) {
+    case 'GET':
+    case 'POST':
+    case 'PUT':
+    case 'DELETE':
+
+    default:
+      return
+  }
   res.status(200).json({ name: 'John Doe' })
 }
